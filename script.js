ddd94db8c251b44860ea35bb01d7b8e3ab5466c8
deceive the world with a lies <98891424+IceHiroHentai@users.noreply.github.com>
2024-04-02 19:05:01 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+
+function plus() {
+    let num1 = parseFloat(document.getElementById('num1').value);
+    let num2 = parseFloat(document.getElementById('num2').value);
+    document.getElementById('result').innerText = "Result: " + (num1 + num2);
+}
+
+function minus() {
+    let num1 = parseFloat(document.getElementById('num1').value);
+    let num2 = parseFloat(document.getElementById('num2').value);
+    document.getElementById('result').innerText = "Result: " + (num1 - num2);
+}
+
+function plication() {
+    let num1 = parseFloat(document.getElementById('num1').value);
+    let num2 = parseFloat(document.getElementById('num2').value);
+    document.getElementById('result').innerText = "Result: " + (num1 * num2);
+}
+
+function antiplication() {
+    let num1 = parseFloat(document.getElementById('num1').value);
+    let num2 = parseFloat(document.getElementById('num2').value);
+    if (num2 === 0) {
+        document.getElementById('result').innerText = "Деление на ноль!";
+    } else {
+        document.getElementById('result').innerText = "Result: " + (num1 / num2);
+    }
+}
+
+function degree() {
+    let num1 = parseFloat(document.getElementById('num1').value);
+    let num2 = parseFloat(document.getElementById('num2').value);
+    document.getElementById('result').innerText = "Result: " + Math.pow(num1, num2);
+}
+
+function antidegree() {
+}
